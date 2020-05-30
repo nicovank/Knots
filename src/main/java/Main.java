63e46f7354e88e3198d64e26c7a89139b32e3ef2
Nicolas van Kempen <nvankemp@gmail.com>
2020-05-29 18:31:00 -0700

    Add progress reports

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,16 +5,23 @@ import java.util.TreeSet;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
+    private static final int PROGRESS_REPORT_THRESHOLD = 300;
+
     public static void main(String[] args) {
         final long start = System.nanoTime();
         final int n = search(Integer.parseInt(args[0]));
         final long elapsed = System.nanoTime() - start;
-        System.out.println("Found " + n + " quandles in " + (elapsed / Math.pow(10, 9)) + " seconds.");
+        System.out.println("Generated " + n + " quandles in " + (elapsed / Math.pow(10, 9)) + " seconds.");
     }
 
     private static int search(int n) {
         final AtomicInteger count = new AtomicInteger(0);
-        Quandle.generate(Z(n), q -> count.getAndIncrement());
+        Quandle.generate(Z(n), q -> {
+            final int current = count.incrementAndGet();
+            if (current % PROGRESS_REPORT_THRESHOLD == 0) {
+                System.out.println("Generated " + current + " quandles...");
+            }
+        });
         return count.get();
     }
 
